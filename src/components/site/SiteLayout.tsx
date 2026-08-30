362011d8f81cfc5467e640550e89d611d6fe2adb
Harsh Raj A <hraj20000@gmail.com>
2026-08-31 02:50:11 +0530

    Remove breadcrumb strip from SiteLayout

--- src/components/site/SiteLayout.tsx
+++ src/components/site/SiteLayout.tsx
@@ -1,16 +1,12 @@
 import type { ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Breadcrumbs from "./Breadcrumbs";
 
 export default function SiteLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="flex-1 pt-16">
-        <Breadcrumbs />
-        <main>{children}</main>
-      </div>
+      <main className="flex-1 pt-16">{children}</main>
       <Footer />
     </div>
   );
