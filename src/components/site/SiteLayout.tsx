a4fd12da7795ee2656b8df6ff018369f34023e8e
Harsh Raj A <hraj20000@gmail.com>
2026-09-01 11:01:01 +0530

    fix: remove breadcrumbs from site layout

--- src/components/site/SiteLayout.tsx
+++ src/components/site/SiteLayout.tsx
@@ -1,8 +1,6 @@
 import type { ReactNode, ReactElement } from "react";
-import { useLocation } from "react-router";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Breadcrumbs from "./Breadcrumbs";
 
 export default function SiteLayout({
   children,
@@ -11,13 +9,9 @@ export default function SiteLayout({
   children: ReactNode;
   hero?: ReactElement;
 }) {
-  const { pathname } = useLocation();
-  const isHome = pathname === "/";
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      {!isHome && <Breadcrumbs />}
       {hero && <div className="relative">{hero}</div>}
       <main className={`flex-1 ${hero ? "" : "pt-16"}`}>{children}</main>
       <Footer />
